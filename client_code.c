6e09c68f54ff2e6d575c893af4c2f15804892b7b
Mihalachi Razvan <razvan3105@yahoo.com>
2021-03-05 14:35:48 +0200

    Client is almost finished

--- client_code.c
+++ client_code.c
@@ -5,7 +5,7 @@
 #include<arpa/inet.h> 
 #include <unistd.h>
 #include<json-c/json.h>
-
+#include <pthread.h>
 #define size 1024
 #define port 8888
 char *ip="127.0.0.1";
@@ -15,7 +15,8 @@ char message[size];
 char server_reply[size];
 char encoded_pass[30]; //parola codificata(cea introdusa de client)
 int chance_count=0;
-
+int sock;
+int leave_flag = 0;
 void encode_pass(char p[30]){
 	int i;
 	for(i=0;i<strlen(p);i++){
@@ -51,20 +52,48 @@ int json_reader(char user[30], char pass[30]){
 		string_password[strlen(string_password)+1]='\0';
 		encode_pass(pass);  //pt a codifica parola introdusa de client => in encoded pass 
 		//daca userul si parola(dupa codificare) introduse de client sunt egale cu cele din json=>succes
-		encoded_pass[strlen(encoded_pass)+1]='\0';
-		
+		encoded_pass[strlen(user)]='\0';
 		if(!strcmp(string_username, user) && !strcmp(string_password, encoded_pass)){
 			return 1;
 		}
 	}
 	return 0;
 }
+void str_overwrite_stdout() {
+  printf("%s", "> ");
+  fflush(stdout);
+}
+void send_msg_handler() {
 
+  while(1) {
+	str_overwrite_stdout();
+	 scanf(" %[^\n]", message);
+	if((send(sock, message, size , 0))<0) {
+		break;
+	}
+    if (strcmp(message, "Exit") == 0) {
+			break;
+    } 
+	bzero(message, size);
+  }
+  leave_flag = 1;
+}
 
-
+void recv_msg_handler() {
+  while (1) {
+		int receive = recv(sock, message, size, 0);
+    if (receive > 0) {
+      printf("%s", message);
+	  str_overwrite_stdout();
+    } else if (receive < 0) {
+			break;
+    } 
+		bzero(message, size);
+  }
+}
 int main(int argc , char *argv[]) {
 
-int sock;
+
 struct sockaddr_in server;
 
 sock = socket(AF_INET , SOCK_STREAM , 0);
@@ -78,7 +107,9 @@ if(sock<0) {
 	exit(-1);
 	
 }
-
+server.sin_addr.s_addr = inet_addr(ip);
+server.sin_family = AF_INET;
+server.sin_port = htons(port); //converteste din formatul intern in cel extern , 2 octteti
 printf("Bun venit");
 printf("\nPentru a te conecta la server te rog sa introduci un username si o parola");
 
@@ -90,83 +121,55 @@ while(chance_count<4 || json_reader(Username, Password)==1){
 if(json_reader(Username, Password)==1){
 		printf("Username-ul si parola sunt corecte!");
 
-	server.sin_addr.s_addr = inet_addr(ip);
- 	server.sin_family = AF_INET;
-    server.sin_port = htons(port); //converteste din formatul intern in cel extern , 2 octteti
-	
-//int connect(int sockfd , const struct sockaddr *addr , socklen_t addrlen)
-
 	  printf("\nmai jos se afla comenzi de care utilizatorul beneficeaza");
 	  printf("\nWho is online  (afiseaza pe ecran , toti utilizatorii aflati in sesiunea)");
 	  printf("\nprivate nume_username (mesajul se afla pe ecranul utlizatorului la care se doreste sa fie transmis)");
 	  printf("\npublic (mesajul se va transmite la toti utilizatorii inscrisi in sesiune)");
-	  printf("\nExit (pentru deconectarea de la sesiune)");
-	  if(connect(sock , (struct sockaddr *)&server,sizeof(server))<0) {
-	
-		  printf("\neroare de conectare la server");
-		  exit(-2);
-	
-	  }
-	  sprintf(message,"%s %s",Username,Password);
-	  if( send(sock,message,strlen(message),0) < 0)
-      	  {
-          	  printf("eroare la trimitire");
-            	exit(-4);
-        	}
-			message[0]='\0';
-			if( recv(sock , server_reply , size , 0) < 0)
-        	{
-				printf("eroare la primire");
-            	exit(-5);        
-        	}
-			printf("\n%s",server_reply);
-			server_reply[0]='\0';
+	  printf("\nExit (pentru deconectarea de la sesiune)\n");
+	  
+						//int connect(int sockfd , const struct sockaddr *addr , socklen_t addrlen)
 
-
- 		   while(1)
-    	   {
-
-        	   printf("Enter message: ");
-			   scanf("%s" , message);
-			   char buffer[2*size];
-			   sprintf(buffer,"%s %s",Username,message);
-        	   if(send(sock , buffer , strlen(buffer),0) < 0)
-        	   {
-            	   printf("eroare la trimitire");
-            	   exit(-3);
-      		 	}
-				buffer[0]='\0';
-				if((strcmp(message,"Exit"))==0) {
-			
-					printf("\nDeconectare de la server");
-					close(sock);
-					exit(1);
-				
+						if(connect(sock , (struct sockaddr *)&server,sizeof(server))<0) {
+							printf("\neroare de conectare la server");
+							exit(-2);
+						}
+	  //sprintf(message,"%s %s",Username,Password);
+	  //if( send(sock,message,strlen(message),0) < 0)
+						if( send(sock,Username,30,0) < 0) {
+							printf("eroare la trimitire");
+							exit(-4);
+						}
+						pthread_t send_msg;
+						if(pthread_create(&send_msg , NULL , (void*)send_msg_handler,NULL) <0 ) {
+						
+							printf("eroare la pthread send_msg\n");
+							exit(-5);
+						
+						}
+						pthread_t recv_msg;
+						if(pthread_create(&recv_msg , NULL , (void*)recv_msg_handler,NULL) <0 ) {
+						
+							printf("eroare la pthread recv_msg\n");
+							exit(-6);
+						
+						}
+					while (1){
+							if(leave_flag){
+							break;
+							}				
+						}
+						close(sock);
+						return 0;
+	  
 				}
-		
-        		if( recv(sock , server_reply , size , 0) < 0)
-       		 {
-				 printf("eroare la primire");
-           	  exit(-4);
-        
-       	 	}
-			printf("Server: %s\n", server_reply);
-			server_reply[0]='\0'; 
-    
-	}
-
-	return 0;
-}
-	else{
+	else {
 		printf("Incercati din nou!");
 		printf("\nUsername:");scanf("%s",Username);
 		printf("Password:");scanf("%s", Password);
-	
-	}
+	   }
 	chance_count++;
 	if(chance_count==4){
-		printf("Numarul de incercari s-a terminat, va rugam incercati mai tarziu :) \n");
+	printf("Numarul de incercari s-a terminat, va rugam incercati mai tarziu :) \n");
 	}
-}
-	
+  }
 }
